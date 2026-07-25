f40df4b88ee4973e93f8a3174fd058d50d0e6316
mieresantonio <mieresantonio@gmail.com>
2026-07-25 00:56:13 +0000

    feat:Completado el proyecto de modelado de datos de Instgram

--- src/models.py
+++ src/models.py
@@ -1,19 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class Usuario(db.Model):
+    __tablename__ = "Usuarios"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(100), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
+
+class Publicacion(db.Model):
+    __tablename__ = "Publicaciones"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("Usuarios.id"))
+
+
+class Comentario(db.Model):
+    __tablename__ = "Comentarios"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    post_id: Mapped[int] = mapped_column(ForeignKey("Publicaciones.id"))
+    author_id: Mapped[int] = mapped_column(ForeignKey("Usuarios.id"))
+    text_coment: Mapped[str] = mapped_column(String(120))
+
+
+class Seguidor(db.Model):
+    __tablename__ = "seguidores"
+
+    usuario_to_id: Mapped[int] = mapped_column(ForeignKey("Usuarios.id"), primary_key=True)
+    usuario_from_id: Mapped[int] = mapped_column(ForeignKey("Usuarios.id"), primary_key=True)
+
+
+class Media(db.Model):
+    __tablename__ = "media"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(String(80), nullable=False)
+    url: Mapped[str] = mapped_column(String(200), nullable=False)
+    Post_id: Mapped[int] = mapped_column(ForeignKey("Publicaciones.id"))
